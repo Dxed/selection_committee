efa7263709f5a92717ebad4416a521e7d8b2d81c
Dxed <dxedmail@gmail.com>
2022-08-02 16:20:01 +0300

    checkbox-query

--- app/script/main.js
+++ app/script/main.js
@@ -1,6 +1,20 @@
+function toggler(id,checkbox, d1="flex",d2="none"){
+        if (checkbox.prop('checked')) {
+             id.css("display",d1)
+             console.log(1)
+            
+        }
+        else{
+            id.css("display",d2)        
+            console.log(2)
 
-$('#other_edu').click(function(){
-    if ($(this).prop('checked')) {
-        alert("Check")
-    }
+        }
+}
+
+$('#other_edu').click(()=>{
+    toggler($("#other_edu_field"),$('#other_edu'));
+});
+
+$('#adres_match').click(()=>{
+    toggler($("#adres_fact_fied"),$('#adres_match'),"none","flex");
 });
